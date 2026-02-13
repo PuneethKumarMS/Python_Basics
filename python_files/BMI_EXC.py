2df73b71b7ea0fba80a25ebd110ec0289c11c094
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-02-13 13:15:44 +0530

    Update BMI_EXC.py

--- python_files/BMI_EXC.py
+++ python_files/BMI_EXC.py
@@ -1,7 +1,6 @@
 w=int(input('enter weight in kg: '))
 h=float(input('enter height in meter: '))
 print(id(w),id(h))
-BMI=(w//h**2)
+BMI=(w//h**2) # Body Mass Index formula
 print(BMI)
 print(id(BMI))
-
