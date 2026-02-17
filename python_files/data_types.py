51cafa99f2a171346afe2443019b3ffceb344753
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-02-17 18:33:22 +0530

    Add array and tuple examples to data_types.py
    Added array and tuple declarations with type prints.

--- python_files/data_types.py
+++ python_files/data_types.py
@@ -2,9 +2,13 @@ a=4.5
 b=2
 var= a<b
 bool = True 
+array = [1,2,3,4]
+tup = (1,2,3,6)
 
-print(var)
 print(type(var))
 print(type(a))
 print(type(b))
 print(type(bool))
+print(type(array))
+print(type(tup))
+
