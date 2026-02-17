638f8169a25c7dfb0819188d1401cff8dd444b58
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-02-17 18:34:37 +0530

    Add dictionary type example to data_types.py

--- python_files/data_types.py
+++ python_files/data_types.py
@@ -4,6 +4,9 @@ var= a<b
 bool = True 
 array = [1,2,3,4]
 tup = (1,2,3,6)
+dict = {1: Puneeth
+        2: Shridhar
+       }
 
 print(type(var))
 print(type(a))
@@ -11,4 +14,4 @@ print(type(b))
 print(type(bool))
 print(type(array))
 print(type(tup))
-
+print(type(dict))
