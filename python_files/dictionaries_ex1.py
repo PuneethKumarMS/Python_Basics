7426c003f89397a87457360de4d863a8785ee012
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-02-23 21:56:36 +0530

    dictionaries

---
+++ python_files/dictionaries_ex1.py
@@ -0,0 +1,9 @@
+cities = { 
+           "Amsterdam": 100,
+           "Mysore": 57301,
+           "Bangalore": 100,
+           "Tokyo": 37400068
+        }
+
+print(cities)
+print(cities["Mysore"])
