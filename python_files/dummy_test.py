66ed8b8f8defa9136cc7fa0c837ad87ca99e9589
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-02-24 13:01:34 +0530

    for_example

--- python_files/dummy_test.py
+++ python_files/dummy_test.py
@@ -1,4 +1,11 @@
-x = 20 
+heights = input('Enter the heights: ').split()
 
-if x > 5 and x <= 20:
- print("ok")
+total = 0
+count = 0
+
+for h in heights:
+    total += int(h)
+    count += 1
+    
+avg = total / count
+print("Average Height:", avg)
