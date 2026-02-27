efcf0451fad57eeca0947805efed6859888a3b35
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-02-27 18:16:49 +0530

    Update for_ex3.py

--- python_files/for_ex3.py
+++ python_files/for_ex3.py
@@ -1,10 +1,8 @@
-
 numbers = [1, 2, 4, 11, -2, 85]
 
 for i in numbers:
     if i % 6  == 0:
         print(i)    
         break
-
 else:
     print("There is no number divisible by 6") # this block would be executed only after the completion of for_loop
